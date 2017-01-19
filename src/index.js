8af4fc2d5500d06cc9400b6198862acd78a10e3c
Naveen Nerusu <Naveen Nerusu>
2017-01-19 06:48:53 -0600

    modified files

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 import VideoList from './components/video-list';
+import VideoDetail from './components/video-details'
 const API_KEY = 'AIzaSyDNRUj4xYmUx7h-xJCM8_HKmozKoxUXBH4';
 
 class App extends Component {
   constructor(props) {
       super(props);
-      this.state = { videos: [] };
+      this.state = {
+        videos: [],
+        selectedVideo: null
+      };
 
-      YTSearch({key: API_KEY, term: 'naveen'}, (videos) => {
-        this.setState({ videos });
-      });
+      this.videoSearch('india');
   }
 
-  render(){
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 400);
+
     return  (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
